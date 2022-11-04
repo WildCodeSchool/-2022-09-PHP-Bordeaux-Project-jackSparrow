451198221b186ced6c73d19689535a5e88e6a930
CodingBDX <antonycharier@gmail.com>
2022-11-04 20:51:45 +0000

    test heroku

---
+++ public/assets/js/cookie.js
@@ -0,0 +1,7 @@
+var like_anime = document.getElementById("like-anime");
+
+like_anime.addEventListener("click", function (event) {
+    event.preventDefault(); // stops modal from being shown
+
+    console.log("zde");
+});
